842df5eb96907f9c4a665e2ca070b554c4805187
1-DAM-T <1-DAM-T>
2016-04-07 19:57:51 +0100

    comentario5

--- DAM_ED04_Actividad/src/dam_ed04_actividad/Empleado.java
+++ DAM_ED04_Actividad/src/dam_ed04_actividad/Empleado.java
@@ -13,4 +13,29 @@ public class Empleado {
     public String nombre;
     public String apellidos;
     public String dni;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
+
