a117c965b7ab3fdafd27f8098501a75238aeccbe
1-DAM-T <1-DAM-T>
2016-04-07 19:51:06 +0100

    comentario3

---
+++ DAM_ED04_Actividad/src/dam_ed04_actividad/Empleado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author 1-DAM-T
+ */
+public class Empleado {
+    public String nombre;
+    public String apellidos;
+    public String dni;
+    public String getNombre() {
+        return nombre;
+    }
+
+   
+    
+}
